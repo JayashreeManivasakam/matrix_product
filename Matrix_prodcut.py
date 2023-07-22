76c1f05cefdd73a8c9262e445a4af633c66ae62e
JayashreeManivasakam <139099541+JayashreeManivasakam@users.noreply.github.com>
2023-07-22 11:49:36 +0530

    Create Matrix_prodcut.py

---
+++ Matrix_prodcut.py
@@ -0,0 +1,11 @@
+n,m=map(int,input().split())
+a=int(input())
+m=[]
+for i in range(n):
+    res=[]
+    for j in range(m):
+        b=int(input())
+        add=b*a
+        res.append(ad)
+    m.append(res) 
+print(m)
